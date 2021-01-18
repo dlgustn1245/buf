78a0e043a5c1944bb5285901a8ef4b33cc1df197
dlgustn1245 <dlgustn1245@naver.com>
2021-01-18 20:39:24 +0900

    A slight modification of SpawnManager

--- LHS_Project/Assets/Scripts/SpawnManager.cs
+++ LHS_Project/Assets/Scripts/SpawnManager.cs
@@ -24,20 +24,18 @@ public class SpawnManager : MonoBehaviour
         randX = Random.Range(xMin, xMax);
         Instantiate(AsteroidPrefab, new Vector2(randX, yPos), Quaternion.identity);
     }
+
     IEnumerator SpawnEnemies()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(startWait);
 
         while (true)
         {
-            for(int i = 0; i < enemies.Length; i++)
-            {
-                randX = Random.Range(xMin, xMax);
-                GameObject enemy = enemies[Random.Range(0, enemies.Length)];
-                Instantiate(enemy, new Vector2(randX, yPos), Quaternion.identity);
+            randX = Random.Range(xMin, xMax + 0.1f);
+            GameObject enemy = enemies[Random.Range(0, enemies.Length)];
+            Instantiate(enemy, new Vector2(randX, yPos), Quaternion.identity);
 
-                yield return new WaitForSeconds(spawnWait);
-            }
+            yield return new WaitForSeconds(spawnWait);
         }
     }
 }
